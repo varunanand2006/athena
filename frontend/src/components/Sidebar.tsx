e636a800dd147ae86c42b9bafd6bbef17b0ee360
Varun <108756946+varunanand2006@users.noreply.github.com>
2026-06-16 15:46:12 -0700

    homepage pushed

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -325,14 +325,6 @@ function IconDocuments() {
   )
 }
 
-function IconMemory() {
-  return (
-    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M12 2a7 7 0 0 0-7 7c0 2 1 3.5 2 4.5V17a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-3.5c1-1 2-2.5 2-4.5a7 7 0 0 0-7-7z" />
-      <line x1="9" y1="22" x2="15" y2="22" />
-    </svg>
-  )
-}
 
 function IconSystem() {
   return (
